6c1f08df0381ed256d7b3d732be0e54c9df91bbf
Asyary Raihan <asyaryraihan@gmail.com>
2022-05-02 03:18:49 +0700

    Init

---
+++ app.js
@@ -0,0 +1,365 @@
+const { Client, MessageMedia, LocalAuth } = require('whatsapp-web.js');
+const express = require('express');
+const { body, validationResult } = require('express-validator');
+const socketIO = require('socket.io');
+const qrcode = require('qrcode');
+const http = require('http');
+const fs = require('fs');
+const { phoneNumberFormatter } = require('./helpers/formatter');
+const fileUpload = require('express-fileupload');
+const axios = require('axios');
+const mime = require('mime-types');
+const moment = require('moment-timezone');
+const schedule = require('node-schedule');
+
+const port = process.env.PORT || 4000;
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(fileUpload({
+  debug: false
+}));
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html', {
+    root: __dirname
+  });
+});
+
+const client = new Client({
+  restartOnAuthFail: true,
+  puppeteer: {
+    headless: true,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-accelerated-2d-canvas',
+      '--no-first-run',
+      '--no-zygote',
+      '--single-process', // <- this one doesn't works in Windows
+      '--disable-gpu'
+    ],
+  },
+  authStrategy: new LocalAuth()
+});
+
+schedule.scheduleJob({rule: "0 3 * * *", tz:"America/Costa_Rica"}, () => {
+  cronReset()
+})
+
+async function cronReset() {
+  let readFile = await fs.readdirSync("./database/")
+  for (let msgFrom of readFile) {
+    fs.unlinkSync("./database/" + msgFrom)
+  }
+}
+
+client.on('message', msg => {
+  // Downloading media
+  if (msg.hasMedia) {
+    msg.downloadMedia().then(media => {
+      if (media) {
+        // The folder to store: change as you want!
+        // Create if not exists
+        const mediaPath = './downloaded-media/';
+
+        if (!fs.existsSync(mediaPath)) {
+          fs.mkdirSync(mediaPath);
+        }
+
+        // Get the file extension by mime-type
+        const extension = mime.extension(media.mimetype);
+        
+        // Filename: change as you want! 
+        // I will use the time for this example
+        // Why not use media.filename? Because the value is not certain exists
+        const filename = new Date().getTime();
+
+        const fullFilename = mediaPath + filename + '.' + extension;
+
+        // Save to file
+        try {
+          fs.writeFileSync(fullFilename, media.data, { encoding: 'base64' }); 
+          console.log('File downloaded successfully!', fullFilename);
+        } catch (err) {
+          console.log('Failed to save the file:', err);
+        }
+      }
+    });
+  }
+
+  // Create database/ if not exist
+  if (!fs.existsSync("./database/")) {
+    fs.mkdirSync("./database/")
+  }
+
+  const lowerChat = msg.body.toLowerCase()
+  const stripFrom = msg.from.replace("@c.us", "")
+
+  if (msg.author == undefined) {
+    // Check if available .(stripFrom) from database/
+    if (!fs.existsSync("./database/." + stripFrom)) {
+      if (!msg.from.includes("@g.us")) {
+        fs.writeFileSync("./database/." + stripFrom, "")
+        msg.reply(`*¡Hola! Reciba un cordial saludo.* 
+
+Soy su Profesional de salud le atenderá el día de hoy, seleccione alguna de las siguientes opciones 
+
+1. Servicios de salud a nivel domiciliar 
+
+2. Servicios de salud a nivel empresarial 
+
+3. Aplicación de Megadosis de Vitamina 
+
+4. Servicios de procedimientos de enfermería 
+
+5.Formar parte de nuestro equipo`)
+      }
+    } else {
+      if (/^[1-4]$/g.test(lowerChat)) {
+        msg.reply("En breve se contactará con un asesor/una asesora.")
+      } else if (lowerChat == "5") {
+        msg.reply("Si desea dejar su Currículum por favor enviar al correo reclutamiento@cuidointegral.com y poner el asunto el puesto al que quiere aplicar")
+      }
+    }
+  }
+});
+
+client.initialize();
+
+// Socket IO
+io.on('connection', function(socket) {
+  socket.emit('message', 'Connecting...');
+
+  client.on('qr', (qr) => {
+    console.log('QR RECEIVED', qr);
+    qrcode.toDataURL(qr, (err, url) => {
+      socket.emit('qr', url);
+      socket.emit('message', 'QR Code received, scan please!');
+    });
+  });
+
+  client.on('ready', () => {
+    socket.emit('ready', 'Whatsapp is ready!');
+    socket.emit('message', 'Whatsapp is ready!');
+  });
+
+  client.on('authenticated', () => {
+    socket.emit('authenticated', 'Whatsapp is authenticated!');
+    socket.emit('message', 'Whatsapp is authenticated!');
+    console.log('AUTHENTICATED');
+  });
+
+  client.on('auth_failure', function(session) {
+    socket.emit('message', 'Auth failure, restarting...');
+  });
+
+  client.on('disconnected', (reason) => {
+    socket.emit('message', 'Whatsapp is disconnected!');
+    client.destroy();
+    client.initialize();
+  });
+});
+
+
+const checkRegisteredNumber = async function(number) {
+  const isRegistered = await client.isRegisteredUser(number);
+  return isRegistered;
+}
+
+// Send message
+app.post('/send-message', [
+  body('number').notEmpty(),
+  body('message').notEmpty(),
+], async (req, res) => {
+  const errors = validationResult(req).formatWith(({
+    msg
+  }) => {
+    return msg;
+  });
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      status: false,
+      message: errors.mapped()
+    });
+  }
+
+  const number = phoneNumberFormatter(req.body.number);
+  const message = req.body.message;
+
+  const isRegisteredNumber = await checkRegisteredNumber(number);
+
+  if (!isRegisteredNumber) {
+    return res.status(422).json({
+      status: false,
+      message: 'The number is not registered'
+    });
+  }
+
+  client.sendMessage(number, message).then(response => {
+    res.status(200).json({
+      status: true,
+      response: response
+    });
+  }).catch(err => {
+    res.status(500).json({
+      status: false,
+      response: err
+    });
+  });
+});
+
+// Send media
+app.post('/send-media', async (req, res) => {
+  const number = phoneNumberFormatter(req.body.number);
+  const caption = req.body.caption;
+  const fileUrl = req.body.file;
+
+  // const media = MessageMedia.fromFilePath('./image-example.png');
+  // const file = req.files.file;
+  // const media = new MessageMedia(file.mimetype, file.data.toString('base64'), file.name);
+  let mimetype;
+  const attachment = await axios.get(fileUrl, {
+    responseType: 'arraybuffer'
+  }).then(response => {
+    mimetype = response.headers['content-type'];
+    return response.data.toString('base64');
+  });
+
+  const media = new MessageMedia(mimetype, attachment, 'Media');
+
+  client.sendMessage(number, media, {
+    caption: caption
+  }).then(response => {
+    res.status(200).json({
+      status: true,
+      response: response
+    });
+  }).catch(err => {
+    res.status(500).json({
+      status: false,
+      response: err
+    });
+  });
+});
+
+const findGroupByName = async function(name) {
+  const group = await client.getChats().then(chats => {
+    return chats.find(chat => 
+      chat.isGroup && chat.name.toLowerCase() == name.toLowerCase()
+    );
+  });
+  return group;
+}
+
+// Send message to group
+// You can use chatID or group name, yea!
+app.post('/send-group-message', [
+  body('id').custom((value, { req }) => {
+    if (!value && !req.body.name) {
+      throw new Error('Invalid value, you can use `id` or `name`');
+    }
+    return true;
+  }),
+  body('message').notEmpty(),
+], async (req, res) => {
+  const errors = validationResult(req).formatWith(({
+    msg
+  }) => {
+    return msg;
+  });
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      status: false,
+      message: errors.mapped()
+    });
+  }
+
+  let chatId = req.body.id;
+  const groupName = req.body.name;
+  const message = req.body.message;
+
+  // Find the group by name
+  if (!chatId) {
+    const group = await findGroupByName(groupName);
+    if (!group) {
+      return res.status(422).json({
+        status: false,
+        message: 'No group found with name: ' + groupName
+      });
+    }
+    chatId = group.id._serialized;
+  }
+
+  client.sendMessage(chatId, message).then(response => {
+    res.status(200).json({
+      status: true,
+      response: response
+    });
+  }).catch(err => {
+    res.status(500).json({
+      status: false,
+      response: err
+    });
+  });
+});
+
+// Clearing message on spesific chat
+app.post('/clear-message', [
+  body('number').notEmpty(),
+], async (req, res) => {
+  const errors = validationResult(req).formatWith(({
+    msg
+  }) => {
+    return msg;
+  });
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      status: false,
+      message: errors.mapped()
+    });
+  }
+
+  const number = phoneNumberFormatter(req.body.number);
+
+  const isRegisteredNumber = await checkRegisteredNumber(number);
+
+  if (!isRegisteredNumber) {
+    return res.status(422).json({
+      status: false,
+      message: 'The number is not registered'
+    });
+  }
+
+  const chat = await client.getChatById(number);
+  
+  chat.clearMessages().then(status => {
+    res.status(200).json({
+      status: true,
+      response: status
+    });
+  }).catch(err => {
+    res.status(500).json({
+      status: false,
+      response: err
+    });
+  })
+});
+
+server.listen(port, function() {
+  console.log('App running on *: ' + port);
+});
